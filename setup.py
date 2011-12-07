3c39fdcaa013dc8564673bd7ea7a610d060e31ba
cekk <andrea.cecchi85@gmail.com>
2011-12-07 10:55:40 +0000

    initial import

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.0'
+
+setup(name='collective.captchacontactinfo',
+      version=version,
+      description="A simple customization for Plone contact-info that add recaptcha for anonymous users",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='plone captcha e-mail contact-info',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.it',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.recaptcha',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
