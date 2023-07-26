a0531eeb81be14e77fde19ae63557602b53d8b60
Daniele Andreotti <daniele.andreotti@redturtle.it>
2023-07-26 15:39:05 +0200

    Added original URL to contact-info form

--- collective/captchacontactinfo/browser/contact_form_view.py
+++ collective/captchacontactinfo/browser/contact_form_view.py
@@ -7,8 +7,13 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.statusmessages.interfaces import IStatusMessage
 from z3c.form import button
 from zope.component import getMultiAdapter
+import zope.schema
+import z3c.form.field
+from z3c.form.interfaces import HIDDEN_MODE
+
 
 import logging
+import warnings
 
 log = logging.getLogger(__name__)
 
@@ -18,6 +23,7 @@ class ContactInfoPolicy(ContactForm):
     template = ViewPageTemplateFile("templates/contact-info.pt")
 
     def updateFields(self):
+
         super(ContactInfoPolicy, self).updateFields()
 
         self.fields["sender_fullname"].field.required = False
@@ -30,3 +36,29 @@ class ContactInfoPolicy(ContactForm):
         self.fields["sender_from_address"].field.description = self.context.translate(
             help_message
         )
+
+        schema_url_field = zope.schema.TextLine(
+            __name__="starting_url",
+            required=False,
+        )
+        orig_url_field = z3c.form.field.Fields(schema_url_field)
+        self.fields += z3c.form.field.Fields(orig_url_field)
+
+        self.fields["starting_url"].field.default = unicode(
+            self.request.environ.get("HTTP_REFERER")
+        )
+        self.fields["starting_url"].mode = HIDDEN_MODE
+
+    def generate_mail(self, variables, encoding=None):
+        template = self.context.restrictedTraverse(self.template_mailview)
+        result = template(self.context, **variables)
+        if encoding is not None:
+            # Maybe someone has customized 'send_message'
+            # and still expects to get an encoded answer back.
+            warnings.warn(
+                "Calling generate_mail with an encoding argument is deprecated. "
+                "You can leave it out, and get text (string) as result.",
+                DeprecationWarning,
+            )
+            result = result.encode(encoding)
+        return result
